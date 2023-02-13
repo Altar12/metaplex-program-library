cfeed7f055c4cd204ec4e87e4588b0399d41253d
Michael Danenberg <56533526+danenbm@users.noreply.github.com>
2023-02-12 23:55:59 -0800

    Add version to verify

--- token-metadata/program/src/processor/collection/verify.rs
+++ token-metadata/program/src/processor/collection/verify.rs
@@ -1,11 +1,21 @@
+use crate::{
+    error::MetadataError,
+    instruction::{Context, Verify, VerifyArgs},
+};
 use solana_program::{account_info::AccountInfo, entrypoint::ProgramResult, pubkey::Pubkey};
 
-use crate::{error::MetadataError, instruction::VerifyArgs};
-
 pub fn verify<'a>(
-    _program_id: &Pubkey,
-    _accounts: &'a [AccountInfo<'a>],
-    _args: VerifyArgs,
+    program_id: &Pubkey,
+    accounts: &'a [AccountInfo<'a>],
+    args: VerifyArgs,
 ) -> ProgramResult {
+    let context = Verify::to_context(accounts)?;
+
+    match args {
+        VerifyArgs::V1 { .. } => verify_v1(program_id, context, args),
+    }
+}
+
+fn verify_v1(_program_id: &Pubkey, _ctx: Context<Verify>, _args: VerifyArgs) -> ProgramResult {
     Err(MetadataError::FeatureNotSupported.into())
 }
